758091387d153c462856cc6183a3e18d8e8308eb
Zach Banks <zach.banks@synthego.com>
2014-08-19 14:09:19 -0700

    Adding vim, xmonad, fish config files

---
+++ config.fish
@@ -0,0 +1,13 @@
+
+function fish_user_key_bindings
+    fish_vi_key_bindings
+    bind -M insert -m insert \cc kill-whole-line force-repaint
+    bind -m insert \cc kill-whole-line force-repaint
+end
+
+function fish_prompt
+    fish_vi_prompt
+end
+
+fish_vi_cursor
+
