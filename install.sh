01f5f722dbfc5de66f6daa105e2012eb014489e1
Zach Banks <zbanks@mit.edu>
2015-06-25 18:30:43 -0400

    Fixing install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ln -s -bi $PWD/vimrc ~/.vimrc
 
-mkdir -p ~/.config/fish/config.fish
+mkdir -p ~/.config/fish/
 ln -sbi $PWD/config.fish ~/.config/fish/config.fish
 
 ln -sbi $PWD/xmonad.hs ~/.xmonad/xmonad.hs
+ln -sbi $PWD/xmobarrc ~/.xmobarrc
 
 sudo ln -sbi $PWD/hosts /etc/hosts
 
